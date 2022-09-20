dc039049451d11c06d3c1a1bc9ed96c2bca7c6bc
gautamsen117 <gautamsen117@gmail.com>
2022-09-20 15:31:22 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,18 @@
+let names = 'chayan';
+switch (names) {
+    case 'raju':
+        console.log('Yes, This is Raju');
+        break;
+    case 'durjoy':
+        console.log('Yes, This is Durjoy');
+        break;
+    case 'rajib':
+        console.log('Yes, This is Rajib');
+        break;
+    case 'saiful':
+        console.log('Yes, This is Saiful');
+        break;
+    default:
+        console.log('Your Name are not available');
+        break;
+}
